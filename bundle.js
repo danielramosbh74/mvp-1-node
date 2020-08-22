588cb77d2e090e9b88fd881cd308ea4a44587673
Daniel Ramos de Carvalho <danielramosbh74@gmail.com>
2020-08-21 22:30:44 -0300

    atualizando

--- bundle.js
+++ bundle.js
@@ -1,20 +1,17 @@
+"use strict";
+
 // var inputElement = document.querySelector('input[name=name1]');
 // var btnElement = document.querySelector('button.button1');
-
 // btnElement.onclick = function() {
 //   var text = inputElement.value;
-
 //   // The only problema is that I don't know how to pass "text" outside the function...
-  
 //   const color = text;
-
 //   if (color === 'blue') {
 //     // console.log (color + ' is my favorite color');
 //     // alert(color + ' is my favorite color');
 //     document.open();
 //     document.write(color + ' is my favorite color');
 //     document.close();
-
 // } else {
 //     // console.log ('I like ' + color + `, but it isn't my favorite color`);
 //     // alert('I like ' + color + ' but it isn`t my favorite color');
@@ -22,16 +19,8 @@
 //     document.write('I like ' + color + ' but it isn`t my favorite color');
 //     document.close();
 // }
-  
 //   alert(color);
-
 // }
-
 // O objetivo inicial agora é apenas testar e focar nos workflows do Node e do NPM no Github Actions
-
-console.log('Testes iniciais');
-// Ok, rodou isso, então quero já montar o pacote para depois ir melhorando e acompanhando o fluxo
+console.log('Testes iniciais'); // Ok, rodou isso, então quero já montar o pacote para depois ir melhorando e acompanhando o fluxo
 // Assim que possível instalar o webpack como dependência de desenvolvimento, mas hoje preciso manter o FOCO em completar o primeiro workflow automatizado no Github Actions
-
-
-
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports {
+    entry: './main.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+    }
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ],
+    },
+};
+
+// Configurações baseadas na aula Webpack Server do Módulo ES6 dos Cursos Starter da Rocketseat
